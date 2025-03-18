d6682f928f6b105c53051e5a0934089ac7687f9d
jasxmm <jasxmmm@gmail.com>
2025-03-18 20:47:55 +0530

    Update OurServices.tsx

--- components/OurServices.tsx
+++ components/OurServices.tsx
@@ -9,23 +9,23 @@ const OurServices: React.FC = () => {
          <div className='w-full h-full flex flex-row border-y'>
             <div className='w-1/2 md:w-1/3 h-full flex flex-col gap-3 p-3 md:p-5 lg:p-10 border-r '>
                <p className='text-lg md:text-xl font-semibold font-questrial'>Social Media Management </p>
-               <p className='text-sm md:text-base'>We provide expert social media consulting, including strategy development, content posting, branding, and page growth. </p>
+               <p className='text-sm md:text-base'>We provide expert social media consulting, including strategy development, content posting, optimisation, and page growth. </p>
             </div>
             <div className='w-1/2 md:w-1/3 h-full flex flex-col gap-3 p-3 md:p-5 lg:p-10 border-r'>
                <p className='text-lg md:text-xl font-semibold font-questrial'>Paid Advertising &  Lead Generation  </p>
-               <p className='text-sm md:text-base'>We help businesses grow by creating targeted ad campaigns that drive engagement, attract quality leads, and maximize conversions. </p>
+               <p className='text-sm md:text-base'>We help businesses grow by creating targeted ad campaigns that attract quality leads, and maximize conversions. </p>
             </div>
             <div className='md:block hidden w-1/3 h-full'>
                <div className='w-full h-full flex flex-col gap-3 p-3 md:p-5 lg:p-10  '>
                   <p className='text-base md:text-lg lg:text-xl font-semibold font-questrial'>Content Creation </p>
-                  <p className='text-sm md:text-base'>We provide full-service content creation, including photography, videography, graphic design, video editing, and photo editing. </p>
+                  <p className='text-sm md:text-base'>We provide full-service content creation, including photography, video production and editing, blog writing, graphic design, etc. </p>
                </div>
             </div>
          </div>
          <div className='w-full h-full flex flex-row'>
             <div className='w-1/2 md:w-1/3 h-full flex flex-col gap-3 p-3 md:p-5 lg:p-10 border-b md:border-b-0 border-r'>
                <p className='text-base md:text-lg lg:text-xl font-semibold font-questrial'>Branding & Design </p>
-               <p className='text-sm md:text-base'>We provide complete graphic design and branding services, including logos, brochures, social media graphics, visiting cards, billboards, and merchandise designs. </p>
+               <p className='text-sm md:text-base'>We provide complete graphic design and branding services, including logo, brochure, social media graphics, business card, and merchandise designs. </p>
             </div>
             <div className='w-1/2 md:w-1/3 h-full flex flex-col gap-3 p-3 md:p-5 lg:p-10 border-b md:border-b-0 border-r '>
                <p className='text-base md:text-lg lg:text-xl font-semibold font-questrial'>Web Design </p>
